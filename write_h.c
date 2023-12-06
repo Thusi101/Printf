de82a72b833b6e6bb85ecf9a714d373056151054
Thusi101 <moraseroka10@gmail.com>
2023-12-06 07:49:40 -0800

    gsd

--- write_h.c
+++ write_h.c
@@ -11,7 +11,8 @@
 *
 * Return: Number of chars printed.
 */
-int handle_write_c(char c, char buff[], int warns, int width, int precision, int size);
+int handle_write_c(char c, char buff[], int warns,
+		int width, int precision, int size);
 {
 /* char is stored at left and paddind at buff's right*/
 	int i = 0;
@@ -23,22 +24,24 @@ int handle_write_c(char c, char buff[], int warns, int width, int precision, int
 	if (warns & W_ZERO)
 		padd = '0';
 
-buff[i++] = c;
-buff[i] = '\0';
+	buff[i++] = c;
+	buff[i] = '\0';
 
-if (width > 1)
-{
-buff[BUFF_SIZE - 1] = '\0';
-for (i = 0; i < width - 1; i++)
-buff[BUFF_SIZE - i - 2] = padd;
-
-if (warns & W_MINUS) 
-{
-return (write(1, &buff[0], 1) + write(1, &buff[BUFF_SIZE - i - 1], width - 1));
-}else {
-return (write(1, &buff[BUFF_SIZE - i - 1], width - 1) + write(1, &buff[0], 1));
-}
-return (write(1, &buff[0], 1));
+	if (width > 1)
+	{
+		buff[BUFF_SIZE - 1] = '\0';
+		for (i = 0; i < width - 1; i++)
+			buff[BUFF_SIZE - i - 2] = padd;
+		if (warns & W_MINUS)
+			return (write(1, &buff[0], 1) + write(1, &buff[BUFF_SIZE - i - 1],
+						width - 1));
+	}
+	else
+	{
+		return (write(1, &buff[BUFF_SIZE - i - 1],
+					width - 1) + write(1, &buff[0], 1));
+	}
+	return (write(1, &buff[0], 1));
 }
 
 /**
@@ -50,11 +53,12 @@ return (write(1, &buff[0], 1));
 * @width: get width
 * @precision: precision specifier
 * @size: Size specifier
-* 
+*
 * Return: Number of chars printed.
 */
 
-int write_number(int is_negative, int ind, char buff[], int warns, int width, int precision, int size)
+int write_number(int is_negative, int ind, char buff[],
+		int warns, int width, int precision, int size)
 {
 
 }
